7d48006233dc6966d1d4e86c879382dd0c1166e0
Lucas Carrias <lucascarrias@outlook.com>
2020-07-23 12:44:44 -0300

    Create blog with routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express'),
       mongoose = require('mongoose'),
-      bodyParser = require('body-parser');
+      bodyParser = require('body-parser'),
+      methodOverride = require('method-override');
 
 var app = express();
 const PORT = 4200;
@@ -19,6 +20,7 @@ mongoose.connect("mongodb://localhost:2717/blog_app", {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"))
 
 //Schema, Model
 var blogSchema = mongoose.Schema({
@@ -45,9 +47,54 @@ app.get("/blogs", (req, res)=>{
             console.log(err);
         else
             res.render("index", {blogs:blogs});
-    });
-    
+    });    
 });
+
+app.get("/blogs/new", (req, res)=>{
+    res.render("new");
+});
+
+app.post("/blogs", (req, res)=> {
+    Blog.create(req.body.blog, (err, blog) =>{
+        if(err)
+            console.log(err);
+        else
+        res.redirect("/blogs");
+    });
+});
+
+app.get("/blogs/:id", (req, res)=>{
+    Blog.findById(req.params.id, (err, foundBlog)=>{
+        if (err)
+            res.redirect("/blogs");
+        else
+            res.render("show", {blog:foundBlog});
+    });
+});
+
+app.get("/blogs/:id/edit", (req, res)=>{
+    Blog.findById(req.params.id, (err, foundBlog)=>{
+        if (err)
+            res.redirect("/blogs");
+        else
+            res.render("edit", {blog:foundBlog});
+    });
+});
+
+app.put("/blogs/:id", (req, res)=>{
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, foundBlog)=>{
+        if (err)
+            res.redirect("/blogs");
+        else
+            res.redirect("/blogs/" + req.params.id);
+    });
+});
+
+app.delete("/blogs/:id", (req, res)=>{
+    Blog.findByIdAndDelete(req.params.id, (err)=> {
+        res.redirect("/blogs");
+    })
+})
 
 app.listen(PORT, "localhost",() => {
     console.log("Server is running.")
